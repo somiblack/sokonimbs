932218cee34d84277092cc367e2067c359bee3ca
somiblack <94022185+somiblack@users.noreply.github.com>
2025-07-12 23:05:32 +0300

    Move dotenv config to top of index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const { createClient } = require('@supabase/supabase-js');
 const TinyPesaAPI = require('./lib/tinypesa');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
